085b6729082bbc514d42730fbdfc060a1e9f8c73
Joni Mustaniemi <jonimustaniemi@MacBook-Pro.local>
2020-07-29 15:02:28 +0300

    Removes unused vars

--- src/components/FormChoiceElement.js
+++ src/components/FormChoiceElement.js
@@ -1,13 +1,9 @@
 import React from "react";
-import ButtonElement from './ButtonElement.js';
-import ModifyMenuElement from './ModifyMenuElement.js';
 
 class FormChoiceElement extends React.Component {
 
-  
     render(props) {
     return <div>
-      
       <div className="choiceElement"></div>
       <h3 className="choiceElementTitle">Type</h3>
     </div>
