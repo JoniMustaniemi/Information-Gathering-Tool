55a78fa6863a005a22bcbe1e5a567d3c4a7e8b40
Joni Mustaniemi <jonimustaniemi@MacBook-Pro.local>
2020-09-11 13:05:15 +0300

    Support for XML

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -225,35 +225,36 @@ class ButtonElement extends React.Component {
       };
       var f = function (f, c, a, s) {
           c.setAttribute("type", t(a));
-          if (t(a) != "array" && t(a) != "object") {
-              if (t(a) != "null") {
+          if (t(a) !== "array" && t(a) !== "object") {
+              if (t(a) !== "null") {
                   c.appendChild(document.createTextNode(a));
               }
           } else {
               for (var k in a) {
                   var v = a[k];
-                  if (k == "__type" && t(a) == "object") {
+                  let ch;
+                  if (k === "__type" && t(a) === "object") {
                       c.setAttribute("__type", v);
                   } else {
-                      if (t(v) == "object") {
-                          var ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
+                      if (t(v) === "object") {
+                          ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
                           f(f, ch, v);
-                      } else if (t(v) == "array") {
-                          var ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
+                      } else if (t(v) === "array") {
+                           ch = c.appendChild(document.createElementNS(null, s ? "item" : k));
                           f(f, ch, v, true);
                       } else {
                           var va = document.createElementNS(null, s ? "item" : k);
-                          if (t(v) != "null") {
+                          if (t(v) !== "null") {
                               va.appendChild(document.createTextNode(v));
                           }
-                          var ch = c.appendChild(va);
+                          ch = c.appendChild(va);
                           ch.setAttribute("type", t(v));
                       }
                   }
               }
           }
       };
-      f(f, c, a, t(a) == "array");
+      f(f, c, a, t(a) === "array");
       return c.outerHTML;
   }
 
